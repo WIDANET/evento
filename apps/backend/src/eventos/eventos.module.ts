771e519ba8cdcdb7cd7faaa884591cd723d1614a
widanet <willianlps99@outlook.com>
2024-11-23 21:12:21 -0300

    update

--- apps/backend/src/eventos/eventos.module.ts
+++ apps/backend/src/eventos/eventos.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { EventosController } from './eventos.controller';
+import { DbModule } from 'src/db/db.module';
 
 @Module({
+  imports: [DbModule],
   controllers: [EventosController]
 })
 export class EventosModule {}
